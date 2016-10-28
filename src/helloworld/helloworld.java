ed233260f0aa29aa53b0174517b7974a1c890663
Steven Williford <steven.williford@pcc.edu>
2016-10-27 20:03:19 -0700

    added main method that prints hello world

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,5 +4,7 @@ package helloworld;
  * Created by StevenW on 10/27/2016.
  */
 public class helloworld {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
