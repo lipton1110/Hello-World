80871b90409a2d5b177f8bea13f8de36bd1b0dfd
Steven Williford <steven.williford@pcc.edu>
2016-10-27 20:19:29 -0700

    print date

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -1,10 +1,11 @@
 package helloworld;
-
+import java.util.Date;
 /**
  * Created by StevenW on 10/27/2016.
  */
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(new Date());
     }
 }
